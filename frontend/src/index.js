a8874917b7e054d76da0acaa882ba29c9b40d358
Lucas Mari <lucasmari17@gmail.com>
2021-08-11 21:21:02 -0300

    Remove webvitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import { AUTH_TOKEN } from './constants';
-import reportWebVitals from './reportWebVitals';
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_BACK_ADDRESS,
@@ -40,8 +39,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
