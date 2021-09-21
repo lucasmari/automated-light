fdea7719aee959515409a05e87b287e592f5596b
Lucas Mari <lucasmari17@gmail.com>
2021-09-21 19:50:54 -0300

    Fix shell and Markdown lint errors

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,27 @@
 set -euo pipefail
 
 NC='\e[0m'
-GREEN='\e[0;32m'
 CYAN='\e[0;36m'
 
 # Setup
 echo -e "\n${CYAN}Setting up...\n${NC}"
 cd infra/setup
 terraform init -reconfigure && terraform apply -auto-approve
-terraform output -raw s3_bucket_id | xargs -i sed -i '/bucket/s/".*"/"{}"/' ../main.tf
+terraform output -raw s3_bucket_id | xargs -I sed -I '/bucket/s/".*"/"{}"/' ../main.tf
 cd ../..
 
 # Build containers and push to ECR
 echo -e "\n${CYAN}Building containers and pushing to ECR...\n${NC}"
-export TF_VAR_account_id=$(aws sts get-caller-identity | jq -r '.Account')
+export TF_VAR_account_id
+TF_VAR_account_id=$(aws sts get-caller-identity | jq -r '.Account')
 aws ecr get-login-password --region us-east-1 \
-| docker login --username AWS --password-stdin $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com
-docker build -t $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com/automated-light:node frontend/
-docker push $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com/automated-light:node
-docker build -t $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com/automated-light:ruby backend/
-docker push $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com/automated-light:ruby
-docker build -t $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com/automated-light:mongo database
-docker push $TF_VAR_account_id.dkr.ecr.us-east-1.amazonaws.com/automated-light:mongo
+| docker login --username AWS --password-stdin "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com
+docker build -t "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com/automated-light:node frontend/
+docker push "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com/automated-light:node
+docker build -t "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com/automated-light:ruby backend/
+docker push "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com/automated-light:ruby
+docker build -t "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com/automated-light:mongo database
+docker push "$TF_VAR_account_id".dkr.ecr.us-east-1.amazonaws.com/automated-light:mongo
 
 # Deploy
 echo -e "\n${CYAN}Deploying...\n${NC}"
--- destroy.sh
+++ destroy.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 NC='\e[0m'
-GREEN='\e[0;32m'
 CYAN='\e[0;36m'
 
 # Destroy
 echo -e "\n${CYAN}Destroying...\n${NC}"
-export TF_VAR_account_id=$(aws sts get-caller-identity | jq -r '.Account')
+export TF_VAR_account_id
+TF_VAR_account_id=$(aws sts get-caller-identity | jq -r '.Account')
 cd infra
 terraform destroy -auto-approve
 cd setup
