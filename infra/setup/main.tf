dd9b30a272232b2bd8aaa717218c17a93af53b9d
Lucas Mari <lucasmari17@gmail.com>
2021-09-22 00:31:52 -0300

    Fix terraform security errors

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -5,33 +5,53 @@ provider "aws" {
 resource "random_uuid" "randomid" {}
 
 #-------------------------------------------------
-#               ===== [ S3 ] =====               
+#               ===== [ ECR ] =====               
 #-------------------------------------------------
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = random_uuid.randomid.result
-  force_destroy = true
-  versioning {
-    enabled = true
+resource "aws_ecr_repository" "repo" {
+  name                 = "automated-light"
+  image_tag_mutability = "IMMUTABLE"
+  encryption_configuration {
+    encryption_type = "KMS"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+  image_scanning_configuration {
+    scan_on_push = true
   }
 
   tags = var.tags
 }
 
 #-------------------------------------------------
-#               ===== [ ECR ] =====               
+#               ===== [ KMS ] =====               
 #-------------------------------------------------
 
-resource "aws_ecr_repository" "repo" {
-  name                 = "automated-light"
-  image_tag_mutability = "MUTABLE"
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+#-------------------------------------------------
+#               ===== [ S3 ] =====               
+#-------------------------------------------------
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = random_uuid.randomid.result
+  force_destroy = true
+  logging {
+    enabled = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   tags = var.tags
 }
